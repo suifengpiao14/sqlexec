006d405058c7cacf27c6f1c6b403cb71fc725824
pz <2912150017@qq.com>
2023-11-19 19:25:41 +0800

    a

--- strearm_currying.go
+++ strearm_currying.go
@@ -8,16 +8,7 @@ import (
 	"github.com/suifengpiao14/stream"
 )
 
-//DBPackHandlersWithSetContent 将DB打包到数据流上下文中
-// func DBPackHandlersWithSetContent(db *sql.DB) (packHandler stream.PackHandler) {
-// 	packHandler = stream.NewPackHandlerWithSetContext(func(ctx context.Context, input []byte) (newCtx context.Context, out []byte, err error) {
-// 		newCtx = setDBToContext(ctx, db)
-// 		return newCtx, input, nil
-// 	}, nil, nil)
-// 	return packHandler
-// }
-
-//MysqlPackHandler 执行sql获取返回
+// MysqlPackHandler 执行sql获取返回
 func MysqlPackHandler(db *sql.DB) (packHandler stream.PackHandler) {
 	packHandler = stream.NewPackHandlerWithSetContext(nil, func(ctx context.Context, input []byte) (out []byte, err error) {
 		sql := string(input)
@@ -37,7 +28,7 @@ func MysqlPackHandler(db *sql.DB) (packHandler stream.PackHandler) {
 	return packHandler
 }
 
-//Byte2StructPackHandler 将mysql数据转换为结构体
+// Byte2StructPackHandler 将mysql数据转换为结构体
 func Byte2StructPackHandler(dst any) (packHandler stream.PackHandler) {
 	packHandler = stream.NewPackHandler(nil, func(ctx context.Context, input []byte) (out []byte, err error) {
 		err = Byte2Struct(input, dst)
