c5d2f0f2212accd3956eae6c4321ee100f011842
pz <2912150017@qq.com>
2024-04-04 17:38:28 +0800

    upgrade

--- sql_exec_test.go
+++ sql_exec_test.go
@@ -13,12 +13,12 @@ import (
 
 func GetExecutorSQL() (executorSql *sqlexec.ExecutorSQL) {
 	dbConfig := sqlexec.DBConfig{
-		DSN: `root:1b03f8b486908bbe34ca2f4a4b91bd1c@mysql(127.0.0.1:3306)/curdservice?charset=utf8&timeout=5s&readTimeout=5s&writeTimeout=5s&parseTime=False&loc=Local&multiStatements=true`,
+		DSN: `root:123@mysql(127.0.0.1:3306)/curdservice?charset=utf8&timeout=5s&readTimeout=5s&writeTimeout=5s&parseTime=False&loc=Local&multiStatements=true`,
 	}
 	sshConfig := &sshmysql.SSHConfig{
-		Address:        "120.24.156.100:2221",
+		Address:        "120.24.156.100:22",
 		User:           "root",
-		PriviteKeyFile: "C:\\Users\\Admin\\.ssh\\id_rsa",
+		PrivateKeyFile: "C:\\Users\\Admin\\.ssh\\id_rsa",
 	}
 
 	executorSql = sqlexec.NewExecutorSQL(dbConfig, sshConfig)
